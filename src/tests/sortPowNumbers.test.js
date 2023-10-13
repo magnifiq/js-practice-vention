2bf65c1b82772cb16d3e0b8937a6997250875a30
magnifiq <git.projectsss@gmail.com>
2023-10-13 14:59:00 +0200

    f

--- src/tests/sortPowNumbers.test.js
+++ src/tests/sortPowNumbers.test.js
@@ -1,5 +1,5 @@
-const  sortPowNumbers  = require("../tasks/sortedPowNumbers.js");
+const sortPowNumbers = require('../tasks/sortedPowNumbers.js')
 
-test("the test for the input [-5,-3,10,7]", ()=>{
-    expect(sortPowNumbers([-5,-3,10,7])).toEqual([9, 25, 49, 100])
-});
+test('the test for the input [-5,-3,10,7]', () => {
+  expect(sortPowNumbers([-5, -3, 10, 7])).toEqual([9, 25, 49, 100])
+})
