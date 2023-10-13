e23008d0447ffdb78c294fbdde3d72d23efb4826
magnifiq <git.projectsss@gmail.com>
2023-10-13 14:32:19 +0200

    fix

--- src/tasks/summEven.js
+++ src/tasks/summEven.js
@@ -1,10 +1,8 @@
-const sumEven = (arr) => {
-  return arr.reduce((sum, curr, idx) => {
-    if (idx % 2 === 1) {
+const sumEven = (arr) => arr.reduce((sum, curr) => {
+    if (curr % 2 === 0) {
       return sum + curr
     }
     return sum
   }, 0)
-}
 
 module.exports = sumEven
